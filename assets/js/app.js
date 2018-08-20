c70a5bbcf856e0c35a8991b53dd4c8496eacd509
Laurent G <laurent@ghirardotti.fr>
2018-08-20 13:54:57 +0200

    Add webpack-encore-pack and config for a single page

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/global.scss');
+
+var $ = require('jquery');
+require('bootstrap');
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
