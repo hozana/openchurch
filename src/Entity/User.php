509f3cc45f1240a7a7535f65fb216dd9cb6c652d
Laurent G <laurent@ghirardotti.fr>
2018-09-04 17:56:34 +0800

    Remove User from API

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,18 +2,12 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
 use FOS\UserBundle\Model\UserInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity
- * @ApiResource(
- *     normalizationContext={"groups"={"user", "user:read"}},
- *     denormalizationContext={"groups"={"user", "user:write"}}
- * )
  */
 class User extends BaseUser
 {
@@ -25,24 +19,12 @@ class User extends BaseUser
     protected $id;
 
     /**
-     * @Groups({"user"})
-     */
-    protected $email;
-
-    /**
      * @ORM\Column(type="string", length=255, nullable=true)
-     * @Groups({"user"})UserInterface
      */
     protected $fullname;
 
-    /**
-     * @Groups({"user:write"})
-     */
     protected $plainPassword;
 
-    /**
-     * @Groups({"user"})
-     */
     protected $username;
 
     public function setFullname(?string $fullname): void
