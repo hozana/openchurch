7ad47d24cd53a84bdb21a6355d91f1901c24f4bb
Laurent G <laurent@ghirardotti.fr>
2018-09-07 18:02:23 +0800

    Fix logs retrieval

--- src/Controller/ChurchHistory.php
+++ src/Controller/ChurchHistory.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Church;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
 
 class ChurchHistory
 {
@@ -17,9 +16,9 @@ class ChurchHistory
 
     public function __invoke(Church $data)
     {
-        $repo = $this->entityManager->getRepository('Gedmo\Loggable\Entity\LogEntry');
-        $church = $repo->find('App\Entity\Church', $data->getId());
-        $logs = $repo->getLogEntries($church);
+        $church = $this->entityManager->find('App\Entity\Church', $data->getId());
+        $logRepo = $this->entityManager->getRepository('Gedmo\Loggable\Entity\LogEntry');
+        $logs = $logRepo->getLogEntries($church);
 
         return $logs;
     }
