812e780c67f3c90f571e737d046f95d9c99f0034
Pierre-Yves Guerder <pierre-yves.guerder@centraliens-lille.org>
2022-06-10 19:19:01 +0200

    Upgrade Node to version 14 + dependencies.

--- openchurch-admin/src/App.js
+++ openchurch-admin/src/App.js
@@ -2,4 +2,4 @@ import React from 'react';
 import './App.css';
 import { HydraAdmin } from '@api-platform/admin';
 
-export default () => <HydraAdmin entrypoint="http://127.0.0.1:8000/api"/>;
+export default () => <HydraAdmin entrypoint="http://127.0.0.1:1819/api/"/>;
