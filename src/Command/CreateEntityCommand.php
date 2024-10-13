259a96bd30977a9941c7f46eb8423a0e5ece8e1f
Quentin Stoeckel <quentin@mesavolt.com>
2024-10-13 10:54:36 +0200

    Add app:create:entity command

---
+++ src/Command/CreateEntityCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Agent;
+use App\Entity\Community;
+use App\Entity\Place;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\String\ByteString;
+
+#[AsCommand(name: 'app:create:entity', description: 'Create a new entity')]
+class CreateEntityCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $entityClassName = $io->choice('Which entity?', [
+            Agent::class,
+            Community::class,
+            Place::class,
+        ]);
+
+        $entity = new $entityClassName();
+
+        if ($entityClassName === Agent::class) {
+            assert($entity instanceof Agent);
+            // Ask some more questions
+            $entity->name = $io->ask('Name?');
+            $entity->apiKey = ByteString::fromRandom(64)->toString();
+        }
+
+        $this->em->persist($entity);
+        $this->em->flush();
+
+        return self::SUCCESS;
+    }
+}
