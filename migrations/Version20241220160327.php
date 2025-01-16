a18706d244433cf6d3242ef6a43cb0df6b318bbc
Cédric Baillet <cedric@hozana.org>
2025-01-16 18:46:37 +0100

    debug ci

--- migrations/Version20241220160327.php
+++ migrations/Version20241220160327.php
@@ -12,7 +12,7 @@ final class Version20241220160327 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $syncroSecretKey = $_ENV['SYNCHRO_SECRET_KEY'] ?? null;
+        $syncroSecretKey = getenv('SYNCHRO_SECRET_KEY') ?? null;
 
         if (!$syncroSecretKey) {
             throw new \RuntimeException('The environment variable SYNCHRO_SECRET_KEY is not defined.');
