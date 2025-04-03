db4660dd1e6a99384996761dc391c3687c6cc142
Cédric Baillet <cedric@hozana.org>
2025-04-03 14:39:33 +0200

    add sentry integration for backend + cli + python

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
